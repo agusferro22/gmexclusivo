4a3b97a143dcfa20f145bd682a0c8d509f1e2ac7
agustinvillagra <agustinvomp@gmail.com>
2024-07-19 16:55:42 -0300

    se cargo productos javascript

---
+++ app.js
@@ -0,0 +1,20 @@
+let productos = [
+    {producto:"buzo adidas", precio:38.000 ,talle:"Xl",color:"celeste",imagen:"img/Enlight6684 (1)-fotor-2024022321226.jpg"},
+    {producto:"buzo nike", precio:42.000 ,talle:"S",color:"negro",imagen:"img/Enlight7000-fotor-20240223204856.jpg"},
+    {producto:"buzo hornets", precio:30.000 ,talle:"M",color:"celeste",imagen:"img/Enlight6780-1-fotor-20240223203133.jpg"},
+    {producto:"conjunto puma", precio:68.000 ,talle:"L",color:"blanco-negro",imagen:"img/Enlight7325-fotor-20240216201912.jpg"},
+    {producto:"conjunto nike", precio:70.000 ,talle:"Xl",color:"negro",imagen:"img/Enlight7365-fotor-20240223203325.jpg"},
+    {producto:"buzo badbunny", precio:35.000 ,talle:"M",color:"beige",imagen:"img/47115252-0625-4d99-9d3c-3e86f562e440-1-fotor-20240223204710.jpg"},
+    {producto:"buzo nikeAir", precio:40.000 ,talle:"xl",color:"beige",imagen:"img/Enlight7206 (1)-fotor-2024022320441.jpg"},
+];
+
+function filtrarProductos(){
+    const categoria =
+    const nombre =
+    const minPrice =
+    const maxPrice =
+};
+
+
+
+
