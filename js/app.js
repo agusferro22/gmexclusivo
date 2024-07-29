1f7b5f086e0641368ca032a9f799b489f6442cf1
agustinvillagra <agustinvomp@gmail.com>
2024-07-29 16:26:47 -0300

    nuevos cambios

--- js/app.js
+++ js/app.js
@@ -1,171 +1,173 @@
-let productos = [
-    {producto:"buzo adidas", precio:38000, talle:"Xl", color:"celeste", imagen:"img/Enlight6684 (1)-fotor-2024022321226.jpg"},
-    {producto:"buzo nike", precio:42000, talle:"S", color:"negro", imagen:"img/Enlight7000-fotor-20240223204856.jpg"},
-    {producto:"buzo hornets", precio:30000, talle:"M", color:"celeste", imagen:"img/Enlight6780-1-fotor-20240223203133.jpg"},
-    {producto:"conjunto puma", precio:68000, talle:"L", color:"blanco-negro", imagen:"img/Enlight7325-fotor-20240216201912.jpg"},
-    {producto:"conjunto nike", precio:70000, talle:"Xl", color:"negro", imagen:"img/Enlight7365-fotor-20240223203325.jpg"},
-    {producto:"buzo badbunny", precio:35000, talle:"M", color:"beige", imagen:"img/47115252-0625-4d99-9d3c-3e86f562e440-1-fotor-20240223204710.jpg"},
-    {producto:"buzo nikeAir", precio:40000, talle:"xl", color:"beige", imagen:"img/Enlight7206 (1)-fotor-2024022320441.jpg"},
-    {producto:"conjunto nikeAir", precio:40000, talle:"L", color:"naranja-gris", imagen:"img/Enlight7366-fotor-20240223204452.jpg"},
+// Productos
+const productos = [
+    { id: "1", categoria: "unisex", producto: "buzo adidas", descripcion: "Buzo cómodo y a la moda", precio: 38000, talle: "Xl", color: "celeste", imagen: "img/Enlight6684 (1)-fotor-2024022321226.jpg" },
+    { id: "2", categoria: "hombre", producto: "buzo nike", descripcion: "Buzo deportivo y casual", precio: 42000, talle: "S", color: "negro", imagen: "img/Enlight7000-fotor-20240223204856.jpg" },
+    { id: "3", categoria: "unisex", producto: "buzo hornets", descripcion: "Buzo estilo Hornets", precio: 30000, talle: "M", color: "celeste", imagen: "img/Enlight6780-1-fotor-20240223203133.jpg" },
+    { id: "4", categoria: "hombre", producto: "conjunto puma", descripcion: "Conjunto deportivo Puma", precio: 68000, talle: "L", color: "blanco-negro", imagen: "img/Enlight7325-fotor-20240216201912.jpg" },
+    { id: "5", categoria: "hombre", producto: "conjunto nike", descripcion: "Conjunto deportivo Nike", precio: 70000, talle: "Xl", color: "negro", imagen: "img/Enlight7365-fotor-20240223203325.jpg" },
+    { id: "6", categoria: "mujer", producto: "buzo badbunny", descripcion: "Buzo Bad Bunny", precio: 35000, talle: "M", color: "beige", imagen: "img/47115252-0625-4d99-9d3c-3e86f562e440-1-fotor-20240223204710.jpg" },
+    { id: "7", categoria: "mujer", producto: "buzo nikeAir", descripcion: "Buzo Nike Air", precio: 40000, talle: "xl", color: "beige", imagen: "img/Enlight7206 (1)-fotor-2024022320441.jpg" },
+    { id: "8", categoria: "unisex", producto: "conjunto nikeAir", descripcion: "Conjunto Nike Air", precio: 40000, talle: "L", color: "naranja-gris", imagen: "img/Enlight7366-fotor-20240223204452.jpg" }
 ];
 
 // Elementos del DOM
-const productoSection = document.getElementById('productos-container');
-const buttonSearch = document.getElementById('searchButton');
-const searchInput = document.getElementById('searchInput');
-const modalProductos = document.getElementById('productosModal');
-const closeModalBtn = document.getElementById('closeModalBtn');
-const closeFooterBtn = document.getElementById('closeFooterBtn');
-const cartButton = document.getElementById('cart');
-const clearCartBtn = document.getElementById('clearCartBtn');
-const checkoutBtn = document.getElementById('checkoutBtn');
-const successMessage = document.getElementById('successMessage');
-const noResultsMessage = document.getElementById('noResultsMessage');
+const elements = {
+    productoSection: document.getElementById('productos-container'),
+    buttonSearch: document.getElementById('searchButton'),
+    searchInput: document.getElementById('searchInput'),
+    modalProductos: document.getElementById('productosModal'),
+    closeModalBtn: document.getElementById('closeModalBtn'),
+    closeFooterBtn: document.getElementById('closeFooterBtn'),
+    cartButton: document.getElementById('cart'),
+    clearCartBtn: document.getElementById('clearCartBtn'),
+    checkoutBtn: document.getElementById('checkoutBtn'),
+    successMessage: document.getElementById('successMessage'),
+    noResultsMessage: document.getElementById('noResultsMessage'),
+    montoCarrito: document.getElementById('monto-carrito'),
+    checkoutModal: document.getElementById('checkoutModal'),
+    closeCheckoutBtn: document.getElementById('closeCheckoutBtn'),
+    cancelCheckoutBtn: document.getElementById('cancelCheckoutBtn'),
+    checkoutForm: document.getElementById('checkoutForm'),
+    paymentMethodSelect: document.getElementById('paymentMethod'),
+    paymentInstallments: document.getElementById('paymentInstallments')
+};
 
-// Función para mostrar los productos en la página principal
-function displayProducts(productos) {
-    productoSection.innerHTML = '';
-    productos.map(producto => {
-        const productoDiv = document.createElement('div');
-        productoDiv.classList.add('grid-container');
-        
-        productoDiv.innerHTML = `
+// Función para renderizar productos en el DOM
+function renderProducts(products) {
+    elements.productoSection.innerHTML = products.map(product => 
+        `<div class="grid-container">
             <div class="item">
                 <figure>
-                    <img src="${producto.imagen}" alt="producto" title="${producto.producto}">
+                    <img src="${product.imagen}" alt="${product.producto}" title="${product.producto}">
                 </figure>
                 <div class="info-product">
-                    <h2>${producto.producto}</h2>
-                    <p class="price">$${producto.precio}</p>
-                    <p class="price">Color: ${producto.color}</p>
-                    <button onclick="addToCart('${producto.producto}')">Añadir al carrito</button>
+                    <h2>${product.producto}</h2>
+                    <p>${product.descripcion}</p>
+                    <p class="price">$${product.precio}</p>
+                    <p class="price">Color: ${product.color}</p>
+                    <p class="price">Categoría: ${product.categoria}</p>
+                    <button onclick="addToCart('${product.id}')">Añadir al carrito</button>
                 </div>
-            </div>`;
-        
-        productoSection.appendChild(productoDiv);
-    });
+            </div>
+        </div>`
+    ).join('');
 }
 
 // Función para añadir un producto al carrito
-function addToCart(producto) {
-    let cart = JSON.parse(localStorage.getItem('productosModal')) || [];
-    cart.push(producto);
-    localStorage.setItem('productosModal', JSON.stringify(cart));
+function addToCart(productId) {
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+    const product = productos.find(p => p.id === productId);
+    cart.push(product);
+    localStorage.setItem('cart', JSON.stringify(cart));
     updateCartCount();
-}
-
-// Función para actualizar el conteo de productos en el carrito
-function updateCartCount() {
-    let cart = JSON.parse(localStorage.getItem('productosModal')) || [];
-    document.getElementById('cuenta-carrito').innerText = cart.length;
-}
-
-// Función para buscar productos
-function searchProducts() {
-    const searchText = searchInput.value.toLowerCase();
-    const filteredProducts = productos.filter(producto => 
-        producto.producto.toLowerCase().includes(searchText) ||
-        producto.color.toLowerCase().includes(searchText)
-    );
-    
-    if (filteredProducts.length > 0) {
-        noResultsMessage.style.display = 'none';
-        displayProducts(filteredProducts);
-    } else {
-        noResultsMessage.style.display = 'block';
-        productoSection.innerHTML = '';
-    }
-}
-
-// Función para abrir el modal de productos
-function openModal() {
-    modalProductos.style.display = 'block';
-    displayModalProducts();
-}
-
-// Función para cerrar el modal de productos
-function closeModal() {
-    modalProductos.style.display = 'none';
-}
-
-// Función para borrar productos del carrito
-function clearCart() {
-    localStorage.removeItem('productosModal');
-    displayModalProducts();
-    updateCartCount();
-    alert('Productos borrados del carrito');
-}
-
-// Función para mostrar los productos en el modal
-function displayModalProducts() {
-    const productosContainer = document.getElementById('productosContainer');
-    let cart = JSON.parse(localStorage.getItem('productosModal')) || [];
-    productosContainer.innerHTML = '';
-    cart.map(producto => {
-        const productoDiv = document.createElement('div');
-        productoDiv.classList.add('grid-container');
-        
-        productoDiv.innerHTML = `
-            <div class="item">
-                <h2>${producto}</h2>
-            </div>`;
-        
-        productosContainer.appendChild(productoDiv);
+    Swal.fire({
+        title: 'Producto añadido al carrito',
+        text: `${product.producto} ha sido añadido al carrito.`,
+        icon: 'success',
+        confirmButtonText: 'Aceptar'
     });
 }
 
-// Función para finalizar la compra
-function checkout() {
-    localStorage.removeItem('productosModal');
-    displayModalProducts();
-    updateCartCount();
-    showSuccessMessage();
+// Función para actualizar el contador del carrito
+function updateCartCount() {
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+    const cartCount = cart.length;
+    document.getElementById('cuenta-carrito').textContent = cartCount;
 }
 
-// Función para mostrar el mensaje de éxito
-function showSuccessMessage() {
-    successMessage.style.display = 'block';
-    setTimeout(() => {
-        successMessage.style.display = 'none';
-    }, 3000); // Oculta el mensaje después de 3 segundos
-}
-
-// Inicialmente mostrar todos los productos
-displayProducts(productos);
-
-// Agregar el evento de búsqueda
-buttonSearch.addEventListener('click', searchProducts);
-
-// Agregar el evento para abrir el modal
-cartButton.addEventListener('click', openModal);
-
-// Agregar el evento para cerrar el modal
-closeModalBtn.addEventListener('click', closeModal);
-closeFooterBtn.addEventListener('click', closeModal);
-
-// Agregar el evento para borrar productos del carrito
-clearCartBtn.addEventListener('click', clearCart);
-
-// Agregar el evento para finalizar la compra
-checkoutBtn.addEventListener('click', checkout);
-
-// Actualizar el conteo del carrito
-updateCartCount();
-
-document.addEventListener("DOMContentLoaded", function() {
-    const bannerContainer = document.getElementById("bannerContainer");
-    
-    const bannerHTML = `
-        <div class="banner">
-            <div class="banner-container">
-                <img src="img/12-fotor-2024030611265.png" alt="banner" class="banner-image">
-                <div class="banner-text">
-                    <h1>Tienda De Ropa</h1>
-                    <p>La culpa de gastar, se acaba al estrenar.</p>
-                </div>
-            </div>
-        </div>
-    `;
-    
-    bannerContainer.innerHTML = bannerHTML;
+// Búsqueda de productos
+elements.buttonSearch.addEventListener('click', () => {
+    const searchTerm = elements.searchInput.value.toLowerCase();
+    const filteredProducts = productos.filter(product => 
+        product.producto.toLowerCase().includes(searchTerm) ||
+        product.descripcion.toLowerCase().includes(searchTerm) ||
+        product.categoria.toLowerCase().includes(searchTerm) ||
+        product.color.toLowerCase().includes(searchTerm)
+    );
+    renderProducts(filteredProducts);
+    elements.noResultsMessage.style.display = filteredProducts.length > 0 ? 'none' : 'block';
 });
+
+// Mostrar productos al cargar la página
+document.addEventListener('DOMContentLoaded', () => {
+    renderProducts(productos);
+    updateCartCount();
+});
+
+// Verificar existencia de elementos antes de añadir eventos
+if (elements.cartButton) {
+    elements.cartButton.addEventListener('click', () => {
+        elements.modalProductos.style.display = 'block';
+        const cart = JSON.parse(localStorage.getItem('cart')) || [];
+        renderCartProducts(cart);
+    });
+}
+
+if (elements.closeModalBtn) {
+    elements.closeModalBtn.addEventListener('click', () => {
+        elements.modalProductos.style.display = 'none';
+    });
+}
+
+if (elements.closeFooterBtn) {
+    elements.closeFooterBtn.addEventListener('click', () => {
+        elements.modalProductos.style.display = 'none';
+    });
+}
+
+if (elements.clearCartBtn) {
+    elements.clearCartBtn.addEventListener('click', () => {
+        localStorage.removeItem('cart');
+        updateCartCount();
+        renderCartProducts([]);
+    });
+}
+
+if (elements.checkoutBtn) {
+    elements.checkoutBtn.addEventListener('click', () => {
+        const cart = JSON.parse(localStorage.getItem('cart')) || [];
+        if (cart.length === 0) {
+            Swal.fire({
+                title: 'Carrito vacío',
+                text: 'No hay productos en el carrito para finalizar la compra.',
+                icon: 'error',
+                confirmButtonText: 'Aceptar'
+            });
+        } else {
+            elements.modalProductos.style.display = 'none';
+            Swal.fire({
+                title: 'Compra finalizada',
+                text: 'Gracias por su compra.',
+                icon: 'success',
+                confirmButtonText: 'Aceptar'
+            }).then(() => {
+                localStorage.removeItem('cart');
+                updateCartCount();
+                renderCartProducts([]);
+            });
+        }
+    });
+}
+
+// Función para renderizar productos en el carrito
+function renderCartProducts(cartProducts) {
+    const cartContainer = document.getElementById('productosContainer');
+    if (cartContainer) {
+        cartContainer.innerHTML = cartProducts.map(product => 
+            `<div class="grid-container">
+                <div class="item">
+                    <figure>
+                        <img src="${product.imagen}" alt="${product.producto}" title="${product.producto}">
+                    </figure>
+                    <div class="info-product">
+                        <h2>${product.producto}</h2>
+                        <p>${product.descripcion}</p>
+                        <p class="price">$${product.precio}</p>
+                        <p class="price">Color: ${product.color}</p>
+                        <p class="price">Categoría: ${product.categoria}</p>
+                    </div>
+                </div>
+            </div>`
+        ).join('');
+    }
+}
